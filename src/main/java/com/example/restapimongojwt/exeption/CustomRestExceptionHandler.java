88699c1ec0ba39e02369ed5239130b795dfcb5a2
KovachVolodymer <kovach.volodymyr1@student.uzhnu.edu.ua>
2024-02-24 16:58:37 +0200

    add new ExceptionHandler

--- src/main/java/com/example/restapimongojwt/exeption/CustomRestExceptionHandler.java
+++ src/main/java/com/example/restapimongojwt/exeption/CustomRestExceptionHandler.java
@@ -2,11 +2,13 @@ package com.example.restapimongojwt.exeption;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
@@ -35,7 +37,21 @@ public class CustomRestExceptionHandler {
                 .collect(Collectors.toList());
 
         body.put("errors", errors);
-
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<String> handleNoHandlerFoundException(NoHandlerFoundException e) {
+        return new ResponseEntity<>("Ендпоінт не знайдено: " + e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return new ResponseEntity<>("Внутрішня помилка сервера: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException e) {
+        return new ResponseEntity<>("Доступ заборонено: " + e.getMessage(), HttpStatus.FORBIDDEN);
     }
 }
